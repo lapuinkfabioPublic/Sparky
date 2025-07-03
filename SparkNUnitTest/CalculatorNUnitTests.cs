823f87c7fbcf3d19f7a5fa0d62bd8ae6fec05d35
lapui <lapui@DESKTOP-8L23PR8>
2025-07-03 17:09:55 -0300

    NUnit e XUnit

---
+++ SparkNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkNUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+
+            Calculator calc = new Calculator();
+
+            //Act
+
+            int result = calc.AddNumber(10, 20);
+
+            //Assert
+            ClassicAssert.AreEqual(30, result);
+        }
+
+    }
+}
