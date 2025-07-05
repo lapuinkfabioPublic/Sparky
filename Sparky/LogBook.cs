ec6df48a59481fb94fc59d2847f90a55258db79f
lapui <lapui@DESKTOP-8L23PR8>
2025-07-05 00:13:45 -0300

    Integration Test Demo

---
+++ Sparky/LogBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public interface ILogBook
+    {
+        void Message(string message);
+    }
+
+    public class LogBook : ILogBook
+    {
+        public LogBook()
+        { }
+
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
