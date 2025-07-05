f4a05e5603171085206d2ea541113ab03fbfb333
lapui <lapui@DESKTOP-8L23PR8>
2025-07-05 00:41:33 -0300

    Bank Account Test

---
+++ SparkNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A2 = Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SparkNUnitTest
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        [Test]
+        public void Setup() {
+            bankAccount = new(new LogBook());
+        }
+
+        [Test]
+        public void BankDeposit_Add100_ReturnTrue() {
+            var result = bankAccount.Deposit(100);
+            A2.Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+
+        }
+
+
+    }
+}
